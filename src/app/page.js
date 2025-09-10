c8dd6ac2de3c37f4b764ed5bfebd04bfbb133cb4
pushkar <pushkargupta606@gmail.com>
2025-09-10 22:38:08 +0530

    changed - added details

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Welcome Srishti and Nash 
+      Mindreader Enterprises is a biotech startup in its establishment phase, dedicated to strategic biotech market analysis and innovation. Our mission is to provide insights into emerging therapies, breakthrough research, and FDA clinical developments to guide investors and industry stakeholders in making informed decisions.
       </main>
       </div>
 
